847ddc1cf2b329b9b12aae6f7a37596edc4783c4
bhavanichary <padilojubhavani>
2022-12-29 22:41:53 +0530

    ex

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter a and b values:\n");
+	scanf("%d%d",&a,&b);
+	printf("the add of two values=%d\n",a+b);
+	printf("the substarction of two values=%d\n",a-b);
+	printf("the multiplication of two values=%d\n",a*b);
+	printf("the division of two values=%d\n",a/b);
+	printf("the modulus of two values=%d\n",a%b);
+}
---
+++ positive.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter the vaule num\n");
+    scanf("%d",&num);
+     if(num>0)
+      {
+      printf("num is positive");
+      }
+
+    else
+     if(num<0)
+      {
+   
+	 printf("num is negative");
+      }
+        {
+	    printf("num is zero");
+	}
+}	 
+
+
